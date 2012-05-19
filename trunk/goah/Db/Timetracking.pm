58e7486795c6d88aeb16e949a113a750499b0302
Tapio Salonsaari <tapio.salonsaari@tietovirta.fi>
2012-05-20 00:47:53 +0300

    Time tracking module is somewhat ready Users can now write their working hours into goah directly, but there's still some missing features which we need to add to make it actually usable.
    Database modification:
    CREATE TABLE Timetracking (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     companyid INTEGER,
     userid INTEGER,
     type INTEGER,
     day DATE,
     description TEXT,
     project TEXT,
     personnel TEXT,
     hours FLOAT
    );
    
    refs 123

---
+++ trunk/goah/Db/Timetracking.pm
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w -CSDA
+
+=begin nd
+
+Package: goah::Db::Timetracking
+
+  An database definition for storing time tracking data. 
+
+About: License
+
+  This software is copyright (c) 2009 by Tietovirta Oy and associates.
+  See LICENSE and COPYRIGHT -files for full details.
+
+=cut
+
+package goah::Db::Timetracking;
+use base qw(goah::Db::Object);
+
+use strict;
+use utf8;
+use warnings;
+
+__PACKAGE__->meta->setup
+        (
+	table      => 'Timetracking',
+	columns    => [ qw(id companyid userid type day description project personnel hours) ],
+	pk_columns => 'id',
+	unique_key => 'id',
+);
+
+1;
