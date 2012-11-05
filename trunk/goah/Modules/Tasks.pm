f953ac93677a44400c707214de00a187a74fbc0a
Tapio Salonsaari <tapio.salonsaari@tietovirta.fi>
2012-11-05 03:54:05 +0200

    First working version of tasks module included into codebase.

--- trunk/goah/Modules/Tasks.pm
+++ trunk/goah/Modules/Tasks.pm
@@ -31,18 +31,27 @@ my $uid;
 my $settref;
 
 my %tasksdb = (
-	0 => { field => 'id', name => 'id', type => 'hidden', required => '0', hidden => 0 },
-	1 => { field => 'companyid', name => __("Customer"), type => 'selectbox', required => '1', data => goah::Modules::Customermanagement->ReadAllCompanies(1), hidden => 0 },
-	2 => { field => 'userid', name => 'userid', type => 'hidden', required => '1', hidden => 0 },
-	3 => { field => 'assigneeid', name => __("Assigned to"), type => 'selectbox', required => '0', data => goah::Modules::Systemsettings->ReadOwnerPersonnel(), hidden => 0 },
-	4 => { field => 'priority', name => __("Priority (1=highest)"), type => 'textfield', required => '0', hiden => 0 },
-	#5 => { field => 'type', name => __('Work type'), type => 'selectbox', required => '1', data => \%timetrackstatuses },
-	6 => { field => 'day', name => __('Date'), type => 'textfield', required => '1', hidden => 1 },
-	7 => { field => 'hours', name => __("Working hours (ie. 1:30 or 1,5)"), type => 'textfield', required => '1', hidden => 0 },
-	8 => { field => 'inthours', name => __("Internal hours (ie. 1:30 or 1,5)"), type => 'textfield', required => '0', hidden => 0 },
+	0 => { field => 'id', name => 'id', type => 'hidden', required => '0', hidden => 1 },
+	1 => { 	field => 'companyid', 
+		name => __("Customer"), 
+		type => 'selectbox', 
+		required => '1', 
+		data => goah::Modules::Customermanagement->ReadAllCompanies(1), 
+		hidden => 0 },
+	2 => { field => 'userid', name => 'userid', type => 'hidden', required => '1', hidden => 1 },
+	3 => { 	field => 'assigneeid', 
+		name => __("Assigned to"), 
+		type => 'selectbox', 
+		required => '0', 
+		data => goah::Modules::Systemsettings->ReadOwnerPersonnel(), 
+		hidden => 0 },
+	4 => { field => 'priority', name => __("Priority"), type => 'textfield', required => '0', hiden => 0, title => "Integer, 1=highest" },
+	6 => { field => 'day', name => __('Date'), type => 'textfield', required => '0', hidden => 1 },
+	7 => { field => 'hours', name => __("Working hrs"), type => 'textfield', required => '1', hidden => 0, title => "1:30 or 1,5" },
+	8 => { field => 'inthours', name => __("Intern. hrs"), type => 'textfield', required => '0', hidden => 0, title => "1:30 or 1,5" },
 	9 => { field => 'description', name => __('Description'),  type => 'textarea', required => '1', hidden => 0 },
 	91 => { field => 'no_billing', name => __("Internal"), type => 'checkbox', required => '0', hidden => 0 },
-	93 => { field => 'longdescription', name => __("Long description, only for internal use"), type => "textarea", required => 0, hidden => 0 },
+	93 => { field => 'longdescription', name => __("Long description, only for internal use"), type => "textarea", required => 0, hidden => 1 },
 	94 => { field => 'completed', name => __("Completed task"), type => 'checkbox', required => 0, hidden => 0 },
 );
 
@@ -92,13 +101,13 @@ sub Start {
 	if($q->param('action')) {
 		
 		if($q->param('action') eq 'writenewtask') {
-			if(WriteTask($uid)) {
+			if(WriteTasks($uid)) {
 				goah::Modules->AddMessage('info',__("Hours tracked"),__FILE__,__LINE__);
 			} else {
 				goah::Modules->AddMessage('error',__("Couldn't write task to database!"),__FILE__,__LINE__);
 			}
 		} elsif($q->param('action') eq 'writeeditedtask') {
-			if(WriteTask($uid)) {
+			if(WriteTasks($uid)) {
 				if($q->param('delete')) {
 					goah::Modules->AddMessage('info',__("Task removed from the database."),__FILE__,__LINE__);
 				} else {
@@ -118,58 +127,28 @@ sub Start {
 			$variables{'dbcompanies'}=goah::Modules::Customermanagement->ReadAllCompanies(1);
 			$variables{'dbusers'}=goah::Modules::Systemsettings->ReadOwnerPersonnel();
 
-			# Get data for actual search, or if no parameters are given, search for hours
-			# at current month
+			$variables{'stateselect'} = {   0 => { key => 'open', value => __("Open tasks") },
+							1 => { key => 'closed', value => __("Closed tasks") },
+							2 => { key => 'all', value => __("All tasks") } };
+
+			# Get data for actual search, or if no parameters are given, search for everything open
 			if($q->param('subaction') && $q->param('subaction') eq 'search' && !($q->param('submit-reset'))) {
 				my $company;
 				my $uid;
-				my $startdate;
-				my $enddate;
-				my $searchdatestart;
-				my $searchdateend;
+				my $stateselect;
 
 				$company = $q->param('customer') if($q->param('customer') && !($q->param('customer')=~/\*/) );
 				$uid = $q->param('user') if($q->param('user'));
-				$startdate = $q->param('fromdate') if($q->param('fromdate'));
-				$enddate = $q->param('todate') if($q->param('todate'));
+				$stateselect = $q->param('stateselect') if ($q->param('stateselect'));
 				
-				if(length($startdate)) {
-					unless($startdate=~/[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}/ || $startdate=~/[0-9]{1,2}\.[0-9]{1,2}/) {
-						goah::Modules->AddMessage('error',__("Start date isn't formatted correctly. Ignoring filter."));
-						$startdate='';
-					} else {
-						my @searchdate=split(/\./,$startdate);
-
-						$searchdate[2]=$yearnow unless($searchdate[2]);
-
-						$searchdatestart=sprintf("%04d-%02d-%02d",$searchdate[2],$searchdate[1],$searchdate[0]);
-						$startdate=sprintf("%02d.%02d.%04d",@searchdate);
-					}
-				} 
-
-				if(length($enddate)) {
-					unless($enddate=~/[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}/ || $enddate=~/[0-9]{1,2}\.[0-9]{1,2}/) {
-						goah::Modules->AddMessage('error',__("End date isn't formatted correctly. Ignoring filter."));
-						$enddate='';
-					} else {
-						my @searchdate=split(/\./,$enddate);
-
-						$searchdate[2]=$yearnow unless($searchdate[2]);
-
-						$searchdateend=sprintf("%04d-%02d-%02d",$searchdate[2],$searchdate[1],$searchdate[0]);
-						$enddate=sprintf("%02d.%02d.%04d",@searchdate);
-					}
-				}
-
-				$variables{'dbdata'}=ReadTasks($uid,$company,$searchdatestart,$searchdateend);
+				$variables{'dbdata'}=ReadTasks($uid,$company,'','',$stateselect);
 				$variables{'search_customer'}=$company;
 				$variables{'search_owners'}=$uid;
-				$variables{'search_startdate'}=$startdate;
-				$variables{'search_enddate'}=$enddate;
+				$variables{'search_stateselect'}=$stateselect;
 				$variables{'search_longdesc'}='checked' if($q->param('search_longdesc'));
 
 			} else {
-				$variables{'dbdata'}=ReadTasks('','','','','yes','unimported');
+				$variables{'dbdata'}=ReadTasks('','','','','open');
 			}
 
                 } else {
@@ -180,7 +159,7 @@ sub Start {
 	}
 
 	if($variables{'function'}=~/modules\/Tasks\/tasks/) {
-		$variables{'latesthours'}=ReadTasks($uid,'','-20','');
+		$variables{'opentasks'}=ReadTasks($uid,'','-50','');
 	}
 		
 
@@ -248,7 +227,7 @@ sub WriteTasks {
                         goah::Modules->AddMessage('error',$errstr);
 			return 0;
                 } else {
-			if(length($q->param($fieldinfo{'field'})) || $fieldinfo{'field'} eq 'hours' || $fieldinfo{'type'} eq 'checkbox') {
+			if(length($q->param($fieldinfo{'field'})) || $fieldinfo{'field'} eq 'hours' || $fieldinfo{'type'} eq 'checkbox' || $fieldinfo{'field'} eq 'day' ) {
 				my $tmpcol=$fieldinfo{'field'};
 				$dbdata{$tmpcol}=(decode('utf-8',$q->param($fieldinfo{'field'})));
 				
@@ -256,26 +235,53 @@ sub WriteTasks {
 
 					my $hours = $q->param($fieldinfo{'field'});
 					$hours=~s/,/\./g;
-					unless($hours=~/\d+\.?\d*/) {
+					if($hours=~/:/) {
+						my @hoursarr=split(/:/,$hours);
+						my $tmphours=$hoursarr[1]/60;
+						$tmphours+=$hoursarr[0];
+						$hours=$tmphours;
+					} elsif(!$hours=~/\d+\.?\d*/) {
 						goah::Modules->AddMessage('warn',__("Hours column not numeric! Setting hours -value to 0!"));
-						$dbdata{$tmpcol}="0";
+						$hours=0;
 					}
-					if($q->param('minutes')>0) {
-						$dbdata{$tmpcol}+=$q->param('minutes')/60;
+					if($q->param('minutes')) {
+						$hours+=$q->param('minutes')/60;
 					}
+					$dbdata{$tmpcol}=$hours;
 				}	
+				if($fieldinfo{'field'} eq 'inthours') {
+					my $inthours=$q->param($fieldinfo{'field'});
+					$inthours=~s/,/./g;
+					if($inthours=~/:/) {
+						my @inthoursarr=split(/:/,$inthours);
+						my $tmpinthours=$inthoursarr[1]/60;
+						$tmpinthours+=$inthoursarr[0];
+						$inthours=$tmpinthours;
+					} elsif (!$inthours=~/\d+\.?\d*/) {
+						goah::Modules->AddMessage('warn',__("Internal hours column not numeric! Setting value to 0!"));
+						$inthours=0;
+					}
+					if($q->param('intminutes')) {
+						$inthours+=$q->param('intminutes')/60;
+					}
+					$dbdata{$tmpcol}=$inthours;
+				}		
 				if($fieldinfo{'field'} eq 'day') {
 					my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 					$year+=1900;    
 					$mon++; 
-
-					if($q->param('day')=~/[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}/) {
-						$dbdata{$tmpcol}=goah::GoaH::FormatDate($q->param('day')." 00:00:01");
-					} elsif($q->param('day')=~/[0-9]{1,2}\.[0-9]{1,2}/) {
-						$dbdata{$tmpcol}=goah::GoaH::FormatDate($q->param('day').".".$year." 00:00:01");
+					unless($dbdata{$tmpcol}) {
+						$dbdata{$tmpcol}=sprintf("%04d-%02d-%02d",$year,$mon,$mday);
+						goah::Modules->AddMessage("debug","Set date to ".$dbdata{$tmpcol},__FILE__,__LINE__);
 					} else {
+						if($q->param('day')=~/[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}/) {
+							$dbdata{$tmpcol}=goah::GoaH::FormatDate($q->param('day')." 00:00:01");
+						} elsif($q->param('day')=~/[0-9]{1,2}\.[0-9]{1,2}/) {
+							$dbdata{$tmpcol}=goah::GoaH::FormatDate($q->param('day').".".$year." 00:00:01");
+						} else {
 						goah::Modules->AddMessage('warn',__("Incorrectly formatted date!  Using current date."),__LINE__,__FILE__);
 						$dbdata{$tmpcol}=sprintf("%04d-%02d-%02d",$year,$mon,$mday);
+						}
 					}
 
 				}
@@ -321,7 +327,7 @@ sub ReadData {
 
 	shift if ($_[0]=~/goah::Modules::Tasks/);
 
-	unless($_[0]=~/hours/) {
+	unless($_[0]=~/tasks/) {
 		goah::Modules->AddMessage('debug',"ReadData function called with invalid search parameters",__FILE__,__LINE__);
 		return 0;
 	}
@@ -348,13 +354,20 @@ sub ReadData {
 		$field = $tasksdb{$key}{'field'};
 		if($field eq 'day') {
 			$data{$field} = goah::GoaH::FormatDate($datap->$field);
-		} elsif ($field eq 'hours') {
+		} elsif ($field eq 'hours' || $field eq 'inthours') {
 			$data{$field}=$datap->$field;
 			if(!($_[2]) || $_[2] eq '0') {
 				$data{$field}=~s/\.\d*$//;
-				$data{'minutes'}=$datap->$field;
-				$data{'minutes'}=~s/^\d*/0/;
-				$data{'minutes'}=sprintf("%.0f",60*$data{'minutes'});
+				if($field eq 'hours') {
+					$data{'minutes'}=$datap->$field;
+					$data{'minutes'}=~s/^\d*/0/;
+					$data{'minutes'}=sprintf("%.0f",60*$data{'minutes'});
+				} 
+				if($field eq 'inthours') {
+					$data{'intminutes'}=$datap->$field;
+					$data{'intminutes'}=~s/^\d*/0/;
+					$data{'intminutes'}=sprintf("%.0f",60*$data{'intminutes'});
+				}
 			}
 		} else {
 			$data{$field} = $datap->$field;	
@@ -376,8 +389,7 @@ sub ReadData {
 #   1 - customer id's, either single value or array reference
 #   2 - starting day, in YYYY-MM-DD, or negative value to read -1*n last entries
 #   3 - ending day, in YYYY-MM-DD
-#   4 - which hours to read, all (yesno), billable(yes), internal(no), not in basket(open), optional parameter
-#   5 - which hours to read, all (all), ones imported to basket(imported), ones not imported to basket(unimported), optional parameter
+#   4 - which tasks to read, open, closed or all
 #
 # Returns:
 #
@@ -393,7 +405,7 @@ sub ReadTasks {
 	my %dbsearch;
 
 	if($_[0]) {
-		$dbsearch{'userid'}=$_[0];
+		$dbsearch{'assigneeid'}=$_[0];
 		goah::Modules->AddMessage('debug',"Searching with uid ".$dbsearch{'userid'},__FILE__,__LINE__);
 	}
 	if($_[1]) {
@@ -418,41 +430,17 @@ sub ReadTasks {
 		goah::Modules->AddMessage('debug',"Searching with start and end date ".$dbsearch{'day'},__FILE__,__LINE__);
 	}
 
-	# Limit search by billable/internal
+	# Search by state
 	if($_[4]) {
-		if($_[4]=~/^yes$/i) {
-			goah::Modules->AddMessage('debug',"Searching debit hours",__FILE__,__LINE__);
-			$dbsearch{'no_billing'} = "0";
+		if($_[4]=~/open/) {
+			$dbsearch{'completed'} = "0";
 		}
-
-		if($_[4]=~/^no$/i) {
-			goah::Modules->AddMessage('debug',"Searching internal hours",__FILE__,__LINE__);
-			$dbsearch{'no_billing'} = "1";
-		}
-
-		# Limit search for only hours not moved to basket
-		# This implies billable -option
-		if($_[4]=~/^open$/i) {
-			goah::Modules->AddMessage('debug',"Searching open, debit hours",__FILE__,__LINE__);
-			$dbsearch{'no_billing'} = '0';
-			$dbsearch{'or'} = [ basket_id => '', basket_id => 0 ];
-			$dbsearch{'productcode'} = { ne => '' };
+		elsif($_[4]=~/closed/) {
+			goah::Modules->AddMessage('debug',"Searching for completed tasks",__FILE__,__LINE__);
+			$dbsearch{'completed'} = "1";
 		}
 	}
-
-	# Limit search by imported to basket -status
-	if($_[5]) {
-		if($_[5]=~/^unimported$/i) {
-			goah::Modules->AddMessage('debug',"Searching only unimported hours",__FILE__,__LINE__);
-			$dbsearch{'or'}= [ basket_id => '', basket_id => 0 ];
-		}
-
-		if($_[5]=~/^imported$/i) {
-			goah::Modules->AddMessage('debug',"Searching only imported hours",__FILE__,__LINE__);
-			$dbsearch{'or'} = [ basket_id => { gt => 0 }, basket_id => -1 ];
-		}
-	}
-
+		
 	my $datap; 
 	
 	if($_[2]<0) {
@@ -496,34 +484,29 @@ sub ReadTasks {
 					$tdata{$i}{'username'}=__("Not available!");
 				}
 			}
-			if($field eq 'productcode') {
-				my $prodinfop=goah::Modules::Productmanagement->ReadData('products',$row->productcode,$uid,$settref,1);
-				if($prodinfop==0) {
-					$tdata{$i}{'productcode'}=__("Not available!");
-					$tdata{$i}{'productname'}=__("Not available!");
-				} else {
-					my %prodinfo=%$prodinfop;
-					$tdata{$i}{'productcode'}=$prodinfo{'code'};
-					$tdata{$i}{'productname'}=$prodinfo{'name'};
-				}
-			}
-
 			if($field eq 'day') {
 				$tdata{$i}{$field} = goah::GoaH::FormatDate($row->$field);
 			}
-			if ($field eq 'hours') {
+			if ($field eq 'hours' || $field eq 'inthours') {
+				my $minfield='minutes';
+				$minfield='intminutes' if($field eq 'inthours');
+
 				$tdata{$i}{$field}=$row->$field;
 				$tdata{$i}{$field}=~s/\.\d*$//;
-				$tdata{$i}{'minutes'}=$row->$field;
-				$tdata{$i}{'minutes'}=~s/^\d*/0/;
-				if($tdata{$i}{'minutes'} > 0) {
-					$tdata{$i}{'minutes'}=sprintf("%.0f",60*$tdata{$i}{'minutes'});
+				$tdata{$i}{$minfield}=$row->$field;
+				$tdata{$i}{$minfield}=~s/^\d*/0/;
+				if($tdata{$i}{$minfield} > 0) {
+					$tdata{$i}{$minfield}=sprintf("%.0f",60*$tdata{$i}{$minfield});
 				} else {
-					$tdata{$i}{'minutes'}=0;
+					$tdata{$i}{$minfield}=0;
 				}
-				my $billing=1;
-				$billing = 0 if($row->no_billing);
-				$totalhours{$row->type}{$billing}+=$row->$field;
+
+				if($field eq 'inthours') {
+					$totalhours{'internal'}+=$row->$field;
+				} else {
+					$totalhours{'normal'}+=$row->$field;
+					goah::Modules->AddMessage('debug',"Normal hours now ".$totalhours{'normal'}." after +".$row->$field);
+				}
 			}
 			if ($field eq 'longdescription') {
 				$tdata{$i}{$field}=$row->$field;
@@ -540,74 +523,31 @@ sub ReadTasks {
 	goah::Modules->AddMessage('debug',"Got ".($i-10000000)." rows from the database.",__FILE__,__LINE__);
 
 	# Go trough hours and cacl total hours
-	# Warning: Here be dragons.
 	foreach my $t (keys(%totalhours)) {
 		
-		# $t = Row type (normal/night/evening)
-		# -1 index = total sums on this index
-		# 0 index = total for internal hours
-		# 1 index = total for billed hours
+		$tdata{-1}{'normal'}=$totalhours{'normal'};
+		$tdata{-1}{'internal'}=$totalhours{'internal'};
 
-		goah::Modules->AddMessage('debug',"Total hour count for key $t ".$totalhours{$t}{1}."/".$totalhours{$t}{0},__FILE__,__LINE__);
+		$tdata{-1}{'normal'}=~s/\.\d*$//;
+		$tdata{-1}{'internal'}=~s/\.\d*$//;
 
-		$tdata{-1}{$t}{'hours'}{-1}=$totalhours{$t}{0}+$totalhours{$t}{1}; # Total hours for current type
-		$tdata{-1}{-1}{'hours'}{-1}+=$totalhours{$t}{0}+$totalhours{$t}{1}; # Total for all types
-
-		$tdata{-1}{$t}{'hours'}{0}=$totalhours{$t}{0}; # Total internal hours for current type
-		$tdata{-1}{$t}{'hours'}{1}=$totalhours{$t}{1}; # Total billed hours for current ype
-		$tdata{-1}{-1}{'hours'}{0}+=$totalhours{$t}{0}; # Total internal hours for all types
-		$tdata{-1}{-1}{'hours'}{1}+=$totalhours{$t}{1}; # Total billed hours for all types
-
-		$tdata{-1}{$t}{'hours'}{-1}=~s/\.\d*$//; # Total
-		$tdata{-1}{$t}{'hours'}{0}=~s/\.\d*$//; # Internal
-		$tdata{-1}{$t}{'hours'}{1}=~s/\.\d*$//; # Billing
-
-		# Reset values if they're missing
-		$tdata{-1}{$t}{'hours'}{-1}=0 unless($tdata{-1}{$t}{'hours'}{-1});
-		$tdata{-1}{$t}{'hours'}{0}=0 unless($tdata{-1}{$t}{'hours'}{0});
-		$tdata{-1}{$t}{'hours'}{1}=0 unless($tdata{-1}{$t}{'hours'}{1});
+		$tdata{-1}{'normal'}=0 unless($tdata{-1}{'normal'});
+		$tdata{-1}{'internal'}=0 unless($tdata{-1}{'internal'});
 		
 		# Reset variables
-		$tdata{-1}{$t}{'minutes'}{-1}=0;
-		$tdata{-1}{$t}{'minutes'}{0}=0;
-		$tdata{-1}{$t}{'minutes'}{1}=0;
+		$tdata{-1}{'minutes'}=0;
+		$tdata{-1}{'intminutes'}=0;
 
-		# Calculate totals and strip out numbers right from decimal separator
-		$tdata{-1}{$t}{'minutes'}{-1}=$totalhours{$t}{0}+$totalhours{$t}{1};
-		$tdata{-1}{$t}{'minutes'}{0}=$totalhours{$t}{0};
-		$tdata{-1}{$t}{'minutes'}{1}=$totalhours{$t}{1};
+		$tdata{-1}{'minutes'}=$totalhours{'normal'};
+		$tdata{-1}{'intminutes'}=$totalhours{'internal'};
 
-		$tdata{-1}{$t}{'minutes'}{-1}=~s/^\d*/0/;
-		$tdata{-1}{$t}{'minutes'}{0}=~s/^\d*/0/;
-		$tdata{-1}{$t}{'minutes'}{1}=~s/^\d*/0/;
+		$tdata{-1}{'minutes'}=~s/^\d*/0/;
+		$tdata{-1}{'intminutes'}=~s/^\d*/0/;
 
 		# Calculate minutes correctly from decimals
-		$tdata{-1}{$t}{'minutes'}{-1}=sprintf("%.0f",60*$tdata{-1}{$t}{'minutes'}{-1}) if($tdata{-1}{$t}{'minutes'}{-1} > 0);
-		$tdata{-1}{$t}{'minutes'}{0}=sprintf("%.0f",60*$tdata{-1}{$t}{'minutes'}{0}) if($tdata{-1}{$t}{'minutes'}{0} > 0);
-		$tdata{-1}{$t}{'minutes'}{1}=sprintf("%.0f",60*$tdata{-1}{$t}{'minutes'}{1}) if($tdata{-1}{$t}{'minutes'}{1} > 0);
+		$tdata{-1}{'minutes'}=sprintf("%.0f",60*$tdata{-1}{'minutes'}) if ($tdata{-1}{'minutes'} > 0);
+		$tdata{-1}{'intminutes'}=sprintf("%.0f",60*$tdata{-1}{'intminutes'}) if ($tdata{-1}{'intminutes'} > 0);
 	}
-
-	# Reset variables
-	$tdata{-1}{-1}{'minutes'}{-1}=0;
-	$tdata{-1}{-1}{'minutes'}{0}=0;
-	$tdata{-1}{-1}{'minutes'}{1}=0;
-
-	# Total hour count for retrieved hours
-	$tdata{-1}{-1}{'minutes'}{-1}=$tdata{-1}{-1}{'hours'}{-1};
-	$tdata{-1}{-1}{'hours'}{-1}=~s/\.\d*$//;
-	$tdata{-1}{-1}{'minutes'}{-1}=~s/^\d*/0/;
-
-	$tdata{-1}{-1}{'minutes'}{0}=$tdata{-1}{-1}{'hours'}{0};
-	$tdata{-1}{-1}{'hours'}{0}=~s/\.\d*$//;
-	$tdata{-1}{-1}{'minutes'}{0}=~s/^\d*/0/;
-
-	$tdata{-1}{-1}{'minutes'}{1}=$tdata{-1}{-1}{'hours'}{1};
-	$tdata{-1}{-1}{'hours'}{1}=~s/\.\d*$//;
-	$tdata{-1}{-1}{'minutes'}{1}=~s/^\d*/0/;
-
-	$tdata{-1}{-1}{'minutes'}{-1}=sprintf("%.0f",60*$tdata{-1}{-1}{'minutes'}{-1}) if($tdata{-1}{-1}{'minutes'}{-1}>0);
-	$tdata{-1}{-1}{'minutes'}{0}=sprintf("%.0f",60*$tdata{-1}{-1}{'minutes'}{0}) if($tdata{-1}{-1}{'minutes'}{0}>0);
-	$tdata{-1}{-1}{'minutes'}{1}=sprintf("%.0f",60*$tdata{-1}{-1}{'minutes'}{1}) if($tdata{-1}{-1}{'minutes'}{1}>0);
 
 	return \%tdata;
 }
