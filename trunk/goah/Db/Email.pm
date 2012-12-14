fdffecd0b20e15381f52591b38c43dfcd19ce337
Sami Lahtinen <sami.lahtinen@tietovirta.fi>
2012-12-14 16:56:35 +0200

    Converted Tasks email sending as function. Added email sending to deleted tasks.

--- trunk/goah/Db/Email.pm
+++ trunk/goah/Db/Email.pm
@@ -23,7 +23,7 @@ use warnings;
 __PACKAGE__->meta->setup
         (
 	table      => 'Email',
-	columns	   => [ qw(id userid messageid type timestamp delivered from to cc bcc subject body attachments) ],
+	columns	   => [ qw(id userid messageid timestamp type delivered sender recipient cc bcc subject body attachments module) ],
 	pk_columns => 'id',
 	unique_key => 'id',
 );
